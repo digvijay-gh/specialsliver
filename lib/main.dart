c54b112a9528fa72a31be499761b803666b37a20
Digvijay <digvijaydsy3@gmail.com>
2022-12-20 18:37:44 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'homepage.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomePage(),
-    ),
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
